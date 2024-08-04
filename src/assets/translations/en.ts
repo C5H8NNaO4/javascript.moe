2f520267d99cdd1069b82bd4a3a499d946e89a23
Moritz Roessler <moritz.roessler@gmail.com>
2024-08-04 23:38:12 +0200

    feat: update sitemap

--- src/assets/translations/en.ts
+++ src/assets/translations/en.ts
@@ -1,11 +1,9 @@
 export const About = `
-I'm Moe. A guy in his 30ies. I have 11 years of professional experience with JavaScript. 
-Over the years I gained expert knowledge in web and fullstack development. 
-Most of my time I'm working with TypeScript, React and GraphQL.
-As a senior TypeScript developer, I value code quality, static typing and the right 
-tooling. If you're looking for a senior frontend developer or a lead developer 
-in the greater area of Freiburg im Breisgau (or remote), don't hesitate to contact me. 
-I'm well experienced in working with remote teams following Scrum principles.
+I'm Moritz Roessler, a senior TypeScript developer with over 12 years of experience 
+in web and full-stack development. I specialize in JavaScript, TypeScript, React, and GraphQL, 
+focusing on code quality and optimal tooling. 
+My work includes delivering complex projects for large clients, utilizing agile frameworks like Scrum. 
+If you're looking for a lead or senior frontend developer in Freiburg im Breisgau or for remote work, feel free to contact me.
 `;
 
 const translations = {
--- src/lib/routes.tsx
+++ src/lib/routes.tsx
@@ -6,17 +6,23 @@ import { useEffect } from 'react';
 import i18n from "i18next";
 
 
-export const RedirectToLanguage = () => {
+export const RedirectToLanguage = ({ path = '' }) => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        navigate('/' + i18n.language)
+        navigate('/' + i18n.language + path)
     }, [])
 
     return null;
 }
+
+export const RedirectToAboutPage = () => {
+    return <RedirectToLanguage path='/about' />
+}
+
 export const routes = <Routes>
     <Route path="/" Component={RedirectToLanguage} />,
+    <Route path="/about" Component={RedirectToAboutPage} />,
     <Route path="/:language" Component={LandingPage}>
     </Route>
     <Route path="/:language/about" Component={AboutPage} />
