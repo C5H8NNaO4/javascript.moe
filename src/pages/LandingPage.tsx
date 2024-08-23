a4bc9927d3941e3ee1d3da46fdf4a0e60e6eb811
Moritz Roessler <moritz.roessler@gmail.com>
2024-08-23 23:23:46 +0200

    fix: correct translations

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -28,9 +28,6 @@ import { useTranslation } from 'react-i18next';
 
 
 export const LandingPage = () => {
-    const params = new URLSearchParams(window.location.search);
-    const hidden = !!params.get('hidden');
-
     const { scrollYProgress } = useScroll();
     const [swiper, setSwiper] = useState<SwiperClass | null>(null);
 
