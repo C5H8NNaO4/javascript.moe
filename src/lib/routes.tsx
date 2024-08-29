bbe49e44542011fee37e2f620e44805c59d3c79c
Moritz Roessler <moritz.roessler@gmail.com>
2024-08-29 14:11:19 +0200

    fix: update routes

--- src/lib/routes.tsx
+++ src/lib/routes.tsx
@@ -1,32 +1,32 @@
-import { AboutPage } from '@/pages/AboutPage';
-import { LandingPage } from '@/pages/LandingPage';
-import { Route, Routes } from 'react-router';
-import { useNavigate } from 'react-router-dom'
-import { useEffect } from 'react';
+import { AboutPage } from "@/pages/AboutPage";
+import { LandingPage } from "@/pages/LandingPage";
+import { Route, Routes } from "react-router";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import i18n from "i18next";
-import { IngredientPage, PerfumePage } from '@/pages/ParfumPage';
+import { IngredientPage, PerfumePage } from "@/pages/ParfumPage";
 
+export const RedirectToLanguage = ({ path = "" }) => {
+  const navigate = useNavigate();
 
-export const RedirectToLanguage = ({ path = '' }) => {
-    const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/" + i18n.language + path);
+  }, []);
 
-    useEffect(() => {
-        navigate('/' + i18n.language + path)
-    }, [])
-
-    return null;
-}
+  return null;
+};
 
 export const RedirectToAboutPage = () => {
-    return <RedirectToLanguage path='/about' />
-}
+  return <RedirectToLanguage path="/about" />;
+};
 
-export const routes = <Routes>
+export const routes = (
+  <Routes>
     <Route path="/" Component={RedirectToLanguage} />,
     <Route path="/about" Component={RedirectToAboutPage} />,
-    <Route path="/:language" Component={LandingPage}>
-    </Route>
+    <Route path="/:language" Component={LandingPage}></Route>
     <Route path="/:language/about" Component={AboutPage} />
     <Route path="/:languages/perfumes" Component={PerfumePage} />,
-    <Route path="/:languages/perfumes/ingredients" Component={IngredientPage} />,
-</Routes>
+    <Route path="/:languages/perfumes/ingredients" Component={IngredientPage} />
+  </Routes>
+);
