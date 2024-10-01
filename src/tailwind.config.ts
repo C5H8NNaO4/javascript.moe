10a546ce9d2fea1d68fe9beb868da086f91eb119
Moritz Roessler <moritz.roessler@gmail.com>
2024-10-01 21:01:48 +0200

    fix: add missing files

---
+++ src/tailwind.config.ts
@@ -0,0 +1,20 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  theme: {
+    screens: {
+      "3xs": "0px",
+      "2xs": "260px",
+      xs: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {},
+  },  
+  plugins: [],
+};
