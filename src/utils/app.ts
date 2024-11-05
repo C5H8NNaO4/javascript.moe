49d82e054834f5134eb2946f670dea29906244a5
Moritz Roessler <moritz.roessler@gmail.com>
2024-11-05 20:19:16 +0100

    fix: add export and import

---
+++ src/utils/app.ts
@@ -0,0 +1,28 @@
+import { Item } from "@/components/Inventory";
+
+export const importPlainText = (
+  add: (props: Partial<Item>) => void,
+  text: string
+) => {
+  const lines = text
+    .split("\n")
+    .map((l) => {
+      return l?.trim();
+    })
+    .filter(Boolean);
+
+  lines.forEach((line) => {
+    const [amount] = /\d+(g|ml)/.exec(line) || [];
+    const [, title] = /\s(.+?)(?:\s\d+%)?\s?\d+[$€]$/.exec(line) || [];
+    const [, dilution = "100%"] = /\s(\d+%)\s?\d+[$€]/.exec(line) || [];
+    const [, price] = /\s(\d+(€|\$))/.exec(line) || [];
+
+    add({
+      amount,
+      quantity: 1,
+      title: title?.trim(),
+      dilution,
+      price,
+    });
+  });
+};
