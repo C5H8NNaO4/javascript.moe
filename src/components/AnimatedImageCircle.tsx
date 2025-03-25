2016efcbffefa3dbc6579e199c59106911aefc74
Moritz Roessler <moritz.roessler@gmail.com>
2025-03-25 19:43:15 +0100

    fix: refine image circle animation and bd blur

--- src/components/AnimatedImageCircle.tsx
+++ src/components/AnimatedImageCircle.tsx
@@ -1,65 +1,98 @@
-import clsx from 'clsx';
-import { easeIn, easeOut, motion, useScroll, useTransform } from 'framer-motion';
-import { useContext } from 'react';
-import { sectionCtx } from '@/components/AnimatedSection';
+import clsx from "clsx";
+import {
+  easeIn,
+  easeOut,
+  motion,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+import { useContext } from "react";
+import { sectionCtx } from "@/components/AnimatedSection";
 
 export type AnimatedImageCircleProps = {
-    images: string[];
-    alts: string[];
-    className?: string
-    size?: AnimatedImageCircleSize
-}
+  images: string[];
+  alts: string[];
+  className?: string;
+  size?: AnimatedImageCircleSize;
+};
 
 const heights: Record<AnimatedImageCircleSize, number> = {
-    sm: 320,
-}
+  sm: 320,
+};
 
 export enum AnimatedImageCircleSize {
-    sm = 'sm'
+  sm = "sm",
 }
 
-export const AnimatedImageCircle = ({ images, className, size = AnimatedImageCircleSize.sm, alts }: AnimatedImageCircleProps) => {
-    const clsn = clsx("relative rounded-full overflow-hidden my-auto", className)
-    const imgHeight = heights[size];
+export const AnimatedImageCircle = ({
+  images,
+  className,
+  size = AnimatedImageCircleSize.sm,
+  alts,
+}: AnimatedImageCircleProps) => {
+  const clsn = clsx("relative rounded-full overflow-hidden my-auto", className);
+  const imgHeight = heights[size];
 
-    const { ref } = useContext(sectionCtx);
-    const { scrollYProgress } = useScroll({
-        layoutEffect: false,
-        target: ref || undefined,
-        offset: ["start start", "end end"]
-    });
+  const { ref } = useContext(sectionCtx);
+  const { scrollYProgress } = useScroll({
+    layoutEffect: false,
+    target: ref || undefined,
+    offset: ["start start", "end end"],
+  });
 
-    const imgFilter = useTransform(scrollYProgress, [0, 0.25], ["saturate(100%)", "saturate(30%)"])
-    const opacity = useTransform(scrollYProgress, [0.2, 0.33], ["100%", "0%"], { ease: easeOut })
-    const reverse = useTransform(scrollYProgress, [0.2, 0.33], ["0%", "100%"], { ease: easeIn })
+  const imgFilter = useTransform(
+    scrollYProgress,
+    [0, 0.25],
+    ["saturate(100%)", "saturate(30%)"]
+  );
+  const opacity = useTransform(scrollYProgress, [0.2, 0.27], ["80%", "10%"], {
+    ease: easeOut,
+  });
+  const reverse = useTransform(scrollYProgress, [0.21, 0.33], ["0%", "100%"], {
+    ease: easeIn,
+  });
 
-    return <motion.div className={clsn} style={{
-        borderRadius: '1000px',
-        height: imgHeight + 'px',
-        width: imgHeight + 'px',
+  return (
+    <motion.div
+      className={clsn}
+      style={{
+        borderRadius: "1000px",
+        height: imgHeight + "px",
+        width: imgHeight + "px",
         filter: imgFilter,
-        backdropFilter: 'blur(12px)',
-        boxShadow: '0px 0px 6px 1px black',
+        boxShadow: "0px 0px 6px 1px black",
         // opacity,
+        backdropFilter: "blur(2px)",
 
-        zIndex: 100,
-    }}>
-        <motion.img
-            width={imgHeight + 'px'}
-            height={imgHeight + 'px'}
-            src={images[0]} loading="eager"
-            className="absolute h-full w-full"
-            style={{ opacity, objectFit: 'cover', height: '100%' }}
-            alt={alts[0]}
-        />
-        <motion.img
-            width={imgHeight + 'px'}
-            height={imgHeight + 'px'}
-            loading="lazy"
-            src={images[1]}
-            className="absolute h-full w-full"
-            style={{ opacity: reverse, objectFit: 'cover', height: '100%' }}
-            alt={alts[1]}
-        />
+        zIndex: 0,
+      }}
+    >
+      <motion.img
+        width={imgHeight + "px"}
+        height={imgHeight + "px"}
+        src={images[0]}
+        loading="eager"
+        className="absolute h-full w-full"
+        style={{
+          opacity,
+          objectFit: "cover",
+          height: "100%",
+        }}
+        alt={alts[0]}
+      />
+      <motion.img
+        width={imgHeight + "px"}
+        height={imgHeight + "px"}
+        loading="lazy"
+        src={images[1]}
+        className="absolute h-full w-full"
+        style={{
+          opacity: reverse,
+          objectFit: "cover",
+          height: "100%",
+        }}
+        alt={alts[1]}
+      />
     </motion.div>
-}
+  );
+};
