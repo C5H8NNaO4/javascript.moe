3df4ca36d4beb38c9b1ac30cacb88ca8531df0e8
Moritz Roessler <moritz.roessler@gmail.com>
2025-04-02 01:31:19 +0200

    fix: linting

--- src/components/Chips/CostPerMlChip.tsx
+++ src/components/Chips/CostPerMlChip.tsx
@@ -4,12 +4,15 @@ import { NormalizedItem } from "libperfumery/dist/types/NormalizedItem";
 
 export const CostPerMlChip = ({ items }: { items: NormalizedItem[] }) => {
   const cost = totalIngredientCostPerMl(items);
-  const formattedCost = formatCurrency(cost, "USD");
+  const formattedCost = formatCurrency(
+    cost > 999 ? Math.round(cost) : cost,
+    "USD"
+  );
   return (
     <Chip
       icon="FaDollarSign"
       className="bg-yellow-500"
-      label={formattedCost + '/ml'}
+      label={formattedCost + "/ml"}
     ></Chip>
   );
 };
